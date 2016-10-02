6b7228856d432bd40ed297c42fa99eb8767b065c
DESKTOP-R1UGAD2\Murakami <snowman14209@gmail.com>
2016-10-03 04:31:32 +0900

    コミットtest

---
+++ GoalSc.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalSc : MonoBehaviour {
+
+    public string Syousya;
+
+    // Use this for initialization
+    void Start () {
+        Syousya = "";
+        Debug.Log("スタートしました！");
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "Player" && Syousya == "") {
+            Syousya = col.gameObject.name;
+            Debug.Log("優勝は" + Syousya + "ちゃんです！");
+        }
+    }
+}
